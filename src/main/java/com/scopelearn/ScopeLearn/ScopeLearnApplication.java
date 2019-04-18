d2a059a536ed24557339c647028490437c45b713
alekharouta <techie.alekh@yahoo.com>
2019-04-19 00:15:00 +0530

    initial

---
+++ src/main/java/com/scopelearn/ScopeLearn/ScopeLearnApplication.java
@@ -0,0 +1,14 @@
+package com.scopelearn.ScopeLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopeLearnApplication {
+
+	public static void main(String[] args) {
+		//hello
+		SpringApplication.run(ScopeLearnApplication.class, args);
+	}
+
+}
