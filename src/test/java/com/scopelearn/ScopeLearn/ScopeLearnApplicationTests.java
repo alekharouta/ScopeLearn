38d862a569d281bd451effd3faf5ee6843dd0c2e
alekharouta <techie.alekh@yahoo.com>
2019-04-19 00:15:23 +0530

    hello

---
+++ src/test/java/com/scopelearn/ScopeLearn/ScopeLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scopelearn.ScopeLearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScopeLearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
